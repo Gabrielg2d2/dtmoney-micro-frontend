8eb0eedb2393045f881e258d04dcd4126396bae8
G2D2 <gabriel.g2d2@gmail.com>
2023-01-08 15:14:03 -0300

    feat: make sut list transactions

--- src/data-layer/transaction/use-cases/list-transactions/list-transactions.test.ts
+++ src/data-layer/transaction/use-cases/list-transactions/list-transactions.test.ts
@@ -1,35 +1,4 @@
-import { ListTransactions } from ".";
-
-type SutTypes = {
-  url?: string;
-  methodGet?: jest.Mock;
-};
-
-const makeSutListTransactions = (params?: SutTypes) => {
-  const urlSpy = params?.url || "url_any";
-  const methodGetSpy =
-    params?.methodGet ||
-    jest.fn().mockResolvedValue({
-      status: 200,
-      data: [
-        {
-          id: "any_id",
-          name: "any_name",
-          price: 10,
-          type: "any_type",
-          category: "any_category",
-        },
-      ],
-    });
-
-  const sut = new ListTransactions(urlSpy, methodGetSpy);
-
-  return {
-    sut,
-    urlSpy,
-    methodGetSpy,
-  };
-};
+import { makeSutListTransactions } from "./makeSut";
 
 describe("ListTransactions", () => {
   it("should call methodGet with correct params", async () => {
