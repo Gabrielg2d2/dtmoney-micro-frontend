efe2c88a31a65195b1bf3f143cb2e4ea84bc2427
G2D2 <gabriel.g2d2@gmail.com>
2023-01-08 12:08:53 -0300

    feat: makeSutUpdateNewTransactions

---
+++ src/data-layer/transaction/use-cases/update-new-transaction/makesut.ts
@@ -0,0 +1,34 @@
+import { UpdateNewTransactions } from ".";
+
+type SutTypes = {
+  urlSpy?: string;
+  methodPutSpy?: jest.Mock;
+  statusSpy?: number;
+};
+
+export const makeSutUpdateNewTransactions = (params?: SutTypes) => {
+  const urlSpy = params?.urlSpy || "url_any";
+  const methodPutSpy =
+    params?.methodPutSpy ||
+    jest.fn().mockResolvedValue({
+      status: 200,
+    });
+  const statusSpy = params?.statusSpy || 200;
+
+  const dataSpy = {
+    name: "any_name",
+    price: 100,
+    type: "deposit",
+    category: "any_category",
+  };
+
+  const sut = new UpdateNewTransactions(urlSpy, methodPutSpy);
+
+  return {
+    sut,
+    urlSpy,
+    methodPutSpy,
+    statusSpy,
+    dataSpy,
+  };
+};
