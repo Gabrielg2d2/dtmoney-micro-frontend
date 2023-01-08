456eec4fd2c8d96b7503056d498f36e2e435133b
G2D2 <gabriel.g2d2@gmail.com>
2023-01-08 14:32:04 -0300

    chore: config jest coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
   transform: {
     "^.+\\.(j|t)sx?$": "babel-jest",
   },
+  coverageDirectory: "../coverage",
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts,tsx}",
+    "!**/node_modules/**",
+    "!*.d.ts",
+    "!**/domain/**",
+    "!root.component.tsx",
+    "!g2d2-dtmoney.tsx",
+  ],
   moduleNameMapper: {
     "\\.(css)$": "identity-obj-proxy",
     "single-spa-react/parcel": "single-spa-react/lib/cjs/parcel.cjs",
