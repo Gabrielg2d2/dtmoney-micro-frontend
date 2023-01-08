faff30071f313aa76ef53e29426d965814464e10
G2D2 <gabriel.g2d2@gmail.com>
2023-01-08 09:11:03 -0300

    feat: system under test AddNewTransaction

---
+++ src/data-layer/transaction/use-cases/add-new-transaction/makeSut.ts
@@ -0,0 +1,27 @@
+import { AddNewTransaction } from ".";
+
+type SutTypes = {
+  url?: string;
+  methodPost?: any;
+  status?: number;
+};
+
+export const makeSutAddNewTransaction = (params?: SutTypes) => {
+  const dataSpy = {
+    name: "any_name",
+    price: 100,
+    type: "deposit",
+    category: "any_category",
+  };
+
+  const url = params?.url || "any_url";
+  const methodPost =
+    params?.methodPost || jest.fn().mockResolvedValue({ status: 200 });
+
+  return {
+    sut: new AddNewTransaction(url, methodPost),
+    urlSpy: url,
+    methodPostSpy: methodPost,
+    dataSpy,
+  };
+};
