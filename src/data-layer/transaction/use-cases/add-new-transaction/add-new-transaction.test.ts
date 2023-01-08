7cd7163b618d013f170611e6274b7daa24dca9b9
G2D2 <gabriel.g2d2@gmail.com>
2023-01-08 09:08:15 -0300

    tests: add new transaction

--- src/data-layer/transaction/use-cases/add-new-transaction/add-new-transaction.test.ts
+++ src/data-layer/transaction/use-cases/add-new-transaction/add-new-transaction.test.ts
@@ -1,7 +1,22 @@
-import { AddNewTransaction } from ".";
+import { makeSutAddNewTransaction } from "./makeSut";
 
 describe("AddNewTransaction", () => {
-  const url = "http://localhost:3000/transactions";
-  const methodPost = jest.fn();
-  const addNewTransaction = new AddNewTransaction(url, methodPost);
+  it("should call methodPost with correct values", async () => {
+    const { sut, urlSpy, methodPostSpy, dataSpy } = makeSutAddNewTransaction();
+
+    await sut.add(dataSpy);
+
+    expect(methodPostSpy).toHaveBeenCalledWith({
+      url: urlSpy,
+      body: dataSpy,
+    });
+  });
+
+  it("should return promise status 200", async () => {
+    const { sut, dataSpy } = makeSutAddNewTransaction();
+
+    const response = await sut.add(dataSpy);
+
+    expect(response.status).toBe(200);
+  });
 });
