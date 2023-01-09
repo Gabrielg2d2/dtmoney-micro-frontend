36e00f3509361f4568d55589b06b6a08abddd2b9
G2D2 <gabriel.g2d2@gmail.com>
2023-01-09 05:45:53 -0300

    feat: mock data_transactions

---
+++ src/mock/transactions.ts
@@ -0,0 +1,16 @@
+export const data_transactions = [
+  {
+    id: "any_id",
+    name: "any_name",
+    price: 10,
+    type: "withdraw",
+    category: "any_category",
+  },
+  {
+    id: "any_id2",
+    name: "any_name2",
+    price: 200,
+    type: "deposit",
+    category: "any_category2",
+  },
+];
